1bab88f5c702374de37cee3971e123486fb54092
Your Name <samanbatool08@gmail.com>
2020-04-28 09:50:10 -0400

    custom data structure

---
+++ struct0.c
@@ -0,0 +1,25 @@
+// using struct.h
+
+#include <cs50.hh>
+#include <stdio.h>
+
+#include "struct.h"
+
+int main(void)
+{
+    // space for students
+    int enrollment = get_int("Enrollment: ");
+    student students[enrollment];
+
+    // same as:
+    string names[enrollment];
+    string dorms[enrollment];
+
+    // prompt for students' names and dorms 
+    for (int i = 0; i < enrollment; i++)
+    {
+        students[i].name = get_string("Name: ");
+        students[i].dorm = get_string("Dorm: ");
+    }
+
+}
